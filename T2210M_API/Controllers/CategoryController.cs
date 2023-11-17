db37825cf270d9285777c06d941086cf129f2287
quanghoatrinh <quanghoa.trinh@gmail.com>
2023-11-17 10:10:24 +0700

    first commit api

---
+++ T2210M_API/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using T2210M_API.Entities;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace T2210M_API.Controllers
+{
+    [ApiController]
+    [Route("/api/category")]
+    public class CategoryController : Controller
+    {
+        private readonly T2210mApiContext _context;
+        public CategoryController(T2210mApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Category> ls = _context.Categories.ToList();
+            return Ok(ls);
+        }
+    }
+}
+
---
+++ T2210M_API/Entities/T2210mApiContext.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace T2210M_API.Entities;
+
+public partial class T2210mApiContext : DbContext
+{
+    public T2210mApiContext()
+    {
+    }
+
+    public T2210mApiContext(DbContextOptions<T2210mApiContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Data Source=localhost, 1433;Database=T2210M_API;User Id=sa;Password=sa123456;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__categori__3214EC0714C68CE9");
+
+            entity.ToTable("categories");
+
+            entity.HasIndex(e => e.Name, "UQ__categori__737584F606CFBC87").IsUnique();
+
+            entity.Property(e => e.Name).HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__products__3214EC076E83A84D");
+
+            entity.ToTable("products");
+
+            entity.Property(e => e.Description).HasColumnType("ntext");
+            entity.Property(e => e.Name).HasMaxLength(255);
+            entity.Property(e => e.Price).HasColumnType("decimal(12, 4)");
+            entity.Property(e => e.Thumbnail)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__products__Catego__3D5E1FD2");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__users__3214EC0783DC59A7");
+
+            entity.ToTable("users");
+
+            entity.HasIndex(e => e.Email, "UQ__users__A9D105349F171C64").IsUnique();
+
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.FullName).HasMaxLength(255);
+            entity.Property(e => e.Password)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Tel)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
